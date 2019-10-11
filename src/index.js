da846420a674e75be69f59f5248154dce0a51b9a
Pavel Burya <pburya@gmail.com>
2019-10-11 13:14:35 +0300

    feat: upload task solution

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+module.exports = function getTimeForEducation(focus = 'family', knowsProgramming = true, config = {family: 4}) {
+    let time = 800 / config[focus];
+    if (knowsProgramming === false) {
+        time += 500 / config[focus];
+    }
+    return Math.ceil(time);
+};
